a99c961ccf96d68e9d66fc9386dfc278d8c451af
jpenot <jpenot@mit.edu>
2025-09-10 16:24:50 -0400

    same

--- Run/bestLinkBudget_bandwidth.m
+++ Run/bestLinkBudget_bandwidth.m
@@ -5,7 +5,6 @@ function [metric, minB, maxB, bandwidths] = bestLinkBudget_bandwidth(X1,X2,XSats
 
     % TLDR: What's the metric that we're optimizing over?
     [~,~,~,bandwidths] = networkAnalysis_bandwidth(X1,X2,"Earth",P2_name,XSats,R,0.9); % Pretty sure alpha needs to be less than 1 since our bandwidths are negative
-    bandwidths = -bandwidths; 
 
     if nargin >= 5
         % Then there is a cap on the feasible datarate passed in
