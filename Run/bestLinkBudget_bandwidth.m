c57f85d4b94583ca894de3a8d04773656c2400b9
jpenot <jpenot@mit.edu>
2025-09-10 16:14:17 -0400

    Fixed bestLinkBudget_bandwidth
    Implements bandwidth cap correctly. Added other metrics (min, max, etc...) for other types of objective functions

--- Run/bestLinkBudget_bandwidth.m
+++ Run/bestLinkBudget_bandwidth.m
@@ -1,15 +1,16 @@
-function metric = bestLinkBudget_bandwidth(X1,X2,XSats,R,maxDR_Mbps,P2_name)
+function [meanB, minB, maxB, bandwidths] = bestLinkBudget_bandwidth(X1,X2,XSats,R,maxDR_Mbps,P2_name)
     if nargin <= 5
         P2_name = "Mars";
     end
 
     % TLDR: What's the metric that we're optimizing over?
     [~,~,~,bandwidths] = networkAnalysis_bandwidth(X1,X2,"Earth",P2_name,XSats,R,0.9); % Pretty sure alpha needs to be less than 1 since our bandwidths are negative
+    bandwidths = -bandwidths; % negative since ga minimizes the objective function
 
     if nargin >= 5
         % Then there is a cap on the feasible datarate passed in
         bandwidths = min(bandwidths, maxDR_Mbps);
     end
 
-    metric = -mean(bandwidths); % negative since ga minimizes the objective function
+    meanB = mean(bandwidths); 
 end
